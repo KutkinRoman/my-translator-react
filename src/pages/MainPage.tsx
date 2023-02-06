ea5b06dd5fe5b2d47220a5f76b6320e272e6cc69
KutkinRoman <kutkin.roman.i@gmail.com>
2023-02-06 20:41:00 +0600

    New Background

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -27,44 +27,60 @@ const MainPage = () => {
     }, [])
 
     return (
-        <WrapperStyled>
-            <NavBar/>
-            <Divider light/>
-            {translatorStores.map((translatorStore, index) => {
-                return (
-                    <React.Fragment>
-                        <ContainerStyled key={`translatorContainer${index}`} maxWidth={'xl'}>
-                            <Translator
-                                index={index}
-                                translatorStore={translatorStore}
-                            />
-                        </ContainerStyled>
-                        <Divider light/>
-                    </React.Fragment>
-                )
-            })}
-            <FooterContainerStyled maxWidth={'xl'}>
-                <Button
-                    variant={'text'}
-                    size={'small'}
-                    color={'secondary'}
-                    fullWidth
-                    onClick={onAddTranslatorHandler}
-                    disabled={translatorStores.length > 100}
-                >
-                    Add LINE
-                </Button>
-            </FooterContainerStyled>
-        </WrapperStyled>
+        <BackgroundColor>
+            <BackgroundImage>
+                <WrapperStyled>
+                    <NavBar/>
+                    <Divider light/>
+                    {translatorStores.map((translatorStore, index) => {
+                        return (
+                            <React.Fragment>
+                                <ContainerStyled key={`translatorContainer${index}`} maxWidth={'xl'}>
+                                    <Translator
+                                        index={index}
+                                        translatorStore={translatorStore}
+                                    />
+                                </ContainerStyled>
+                                <Divider light/>
+                            </React.Fragment>
+                        )
+                    })}
+                    <FooterContainerStyled maxWidth={'xl'}>
+                        <Button
+                            variant={'text'}
+                            size={'small'}
+                            color={'secondary'}
+                            fullWidth
+                            onClick={onAddTranslatorHandler}
+                            disabled={translatorStores.length > 100}
+                        >
+                            Add LINE
+                        </Button>
+                    </FooterContainerStyled>
+                </WrapperStyled>
+            </BackgroundImage>
+        </BackgroundColor>
     );
 };
+
+
+const BackgroundColor = styled('div')(({theme}) => (
+    {
+        background: theme.palette.background.default
+    }
+));
+
+const BackgroundImage = styled('div')({
+    background: 'url(\"https://i.postimg.cc/15qsb794/background-dark.jpg\") no-repeat center',
+    backgroundSize: "cover"
+})
 
 const WrapperStyled = styled('div')(({theme}) => (
     {
         overflowX: 'hidden',
         paddingBottom: 100,
-        background: theme.palette.background.default,
-        minHeight: '100vh'
+        minHeight: '100vh',
+        background: 'linear-gradient(rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0.7),  rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.95) 85%)'
     }
 ));
 
